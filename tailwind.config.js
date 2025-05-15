c0761ef95ead5690a7d878f709dca126e2e2fed7
RYAKAM SAI KUMAR <rakamsai3@gmail.com>
2025-05-15 15:40:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // Make sure this is included
+    './src/**/*.{js,ts,jsx,tsx}', // Include all the React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
